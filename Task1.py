4cbe7c220385419a034992dadd99123b00351c04
egor27032005 <sergeevegor2005@gmail.com>
2023-01-17 17:53:08 -0400

    work

--- Task1.py
+++ Task1.py
@@ -1,59 +1,79 @@
 #выполнял один
-print("хотите добавить запись, введите 1 \n хотите вывести на экран, введите 2")
-print('хотите экспорт, введите 3 \n хотите поменять экспорт введите 5')
-print('хотите импорт введите 4 \n хотите завершить введите NO')
-def add_a_contact(p):
+print("хотите добавить запись, введите add \nхотите вывести на экран, введите concl")
+print('хотите экспорт, введите exp \nхотите импорт введите import')
+print('хотите завершить введите NO')
+def add_a_contact(distributor):
     file=open('справочник.txt', 'a')
-    if p==1:
+    if distributor=='add':
         name=input("введите имя контакта ")
         number=input('введите номер контакта ')
         description=input('введите описание контакта ')
         return file.write('\n'+name+"; "+number+"; "+description)
     file.close()
 
-def conclusion(p):
+def conclusion(distributor):
     file=open('справочник.txt', 'r')
-    if p==2:
+    if distributor=='concl':
         d=file.read()
         return print(d)
     file.close()
 
-def export(p):
-    if p==3:
-        file=open('справочник.txt', 'r')
-        lin=0
-        for line in file:
-            lin += 1
-        lines = [] 
-        with open ('справочник.txt', 'rt') as myfile:
-            for myline in myfile: 
-                lines.append(myline)
-        print("сколько номеров вы хотите забрать? ")
-        pul=int(input())
-        for i in range(pul):##не понимаю, почему он не учитывает i, подскажите пожалуйста
-            print("введите фамилию ")
-            sername=input()
-            for j in range(lin):
-                if sername in lines[j]:
-                    file2=open('наэкспорт.txt', 'a')
-                    return file2.write(lines[j]+'\n')
-                file2.close()
-def change(p):
-    if p==5:
-        file=open('наэкспорт.txt', 'r')
-        map=file.read()
-        file.close
-        map2 = map.split()
-        file2=open("наэкспорт.txt", 'w')
-        print('сколько номеров вы забрали?')
-        n=int(input())
-        for i in range(4*n):
-            file2.write(map2[i]+'\n')
-        file2.close
+def export(distributor):
+    if distributor=="exp":
+        line_count = sum(1 for line in open('справочник.txt'))
+        file = open('справочник.txt', "r")
+        all_sername = []
+        line = file.readline().split()
+        while line:
+            all_sername.extend(line)
+            line = file.readline().split()
+        file.close()
+        file2=open('наэкспорт.txt', 'w')
+        print('Если хотите экспортировать в строчку введте row, если в столбик введите column')
+        view_exp=input()
+        print('введите фамилии, которые хотите exp,чтобы остановиться введите stop.\nесли хотите все, введите all')
+        choos_surnam=input()
+        k=0
+        sername_exp=[]
+        sername=''
+        if choos_surnam!="all":
+            sername_exp.append(choos_surnam)
+            while sername!="stop":
+                sername=input()
+                if sername!="stop":
+                    sername_exp.append(sername)
+                    k=k+1
+            if view_exp=="row":
+                for i in range(k+1):
+                    for j in range(line_count*4):
+                        if sername_exp[i]==all_sername[j]:
+                            for t in range(4):
+                                file2.write(all_sername[j+t]+' ')
+                                if t==3:
+                                    file2.write('\n')
+            elif view_exp=="column":
+                for i in range(k+1):
+                    for j in range(line_count*4):
+                        if sername_exp[i]==all_sername[j]:
+                            for t in range(4):
+                                file2.write(all_sername[j+t]+'\n')
+                        
+        if choos_surnam=='all':
+            if view_exp=='column':
+                for i in range(line_count*4):
+                    file2.write(all_sername[i]+'\n')
+            elif view_exp=="row":
+                for i in range(line_count*4):
+                    file2.write(all_sername[i])
+                    k=k+1
+                    if k%4==0:
+                        file2.write('\n')
 
+        file2.close()
+            
 
-def importing(p):
-    if p==4:
+def importing(distributor):
+    if distributor=="import":
         file=open('импорт.txt', 'r')
         lin=0
         for line in file:
@@ -70,17 +90,15 @@ def importing(p):
             for j in range(lin):
                 if sername in lines[j]:
                     file3=open('справочник.txt', 'a')
-                    return file3.write(lines[j]+'\n')
-                    file3.close()
+                    return file3.write("\n"+lines[j])
+                file3.close()
 
-continuation="YES"   
-while(continuation!="NO"):
-    p=int(input())
-    add_a_contact(p)
-    conclusion(p)
-    export(p)
-    importing(p)
-    change(p)
+distributor=''   
+while(distributor!="NO"):
+    distributor=input()
+    add_a_contact(distributor)
+    conclusion(distributor)
+    export(distributor)
+    importing(distributor)
     print("желаете еще что-то? ")
-    continuation=input()
      
